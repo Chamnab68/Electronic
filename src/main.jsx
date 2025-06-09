e65db4cd498d562db0e5846df2d45dfb20f34ad4
Chamnab68 <sambathchamnab57@gmail.com>
2025-06-09 09:46:46 +0700

    last

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}>
+            <Route path="/" element={<Home />} />
             <Route path="home" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
